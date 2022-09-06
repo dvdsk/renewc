7ba6947590f4dedbe91b2b6aaea1e43df30a7d57
dvdsk <noreply@davidsk.dev>
2022-09-06 11:52:34 +0200

    init commit

---
+++ crosscompile.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+TARGET_CC=$(pwd)/aarch64-linux-musl-cross/bin/aarch64-linux-musl-gcc
+TARGET_AR=$(pwd)/aarch64-linux-musl-cross/bin/aarch64-linux-musl-ar
+
+TARGET_CC=$TARGET_CC cargo b --target aarch64-unknown-linux-musl --example high_level
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use clap::Parser;
+
+mod cert;
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Domains
+    #[clap(short, required = true)]
+    domains: Vec<String>,
+
+    /// Contact info
+    #[clap(short)]
+    email: Vec<String>,
+
+    /// Use Let's Encrypt production environment
+    /// (see https://letsencrypt.org/docs/staging-environment/)
+    #[clap(long)]
+    prod: bool,
+
+    #[clap(short, long, default_value = "443")]
+    port: u16,
+}
+
+#[tokio::main]
+async fn main() {
+    simple_logger::init_with_level(log::Level::Info).unwrap();
+}
+
