061c00d34d48498022b4c49fb8d31e8d174c1a13
dvdsk <noreply@davidsk.dev>
2022-09-29 00:08:56 +0200

    replace build.rs with setup_crosscompiler binary

--- crosscompile.sh
+++ crosscompile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TARGET_CC=$(pwd)/aarch64-linux-musl-cross/bin/aarch64-linux-musl-gcc
-TARGET_AR=$(pwd)/aarch64-linux-musl-cross/bin/aarch64-linux-musl-ar
-
-TARGET_CC=$TARGET_CC cargo b --target aarch64-unknown-linux-musl --release
+cd setup_crosscompile
+cargo r
+cd ..
+cargo b --target aarch64-unknown-linux-musl --release
---
+++ setup_crosscompile/src/main.rs
@@ -0,0 +1,25 @@
+// #[cfg(target_arch = "aarch64")]
+fn setup_aarch_crosscomp() {
+    use flate2::bufread::GzDecoder;
+    use std::io::BufReader;
+    use std::path::Path;
+    use tar::Archive;
+
+    #[cfg(not(all(target_arch = "x86_64", target_os = "linux")))]
+    compile_error!("only setup to download crosscomp running on x86_64 linux");
+
+    let aarch_musl_cross = Path::new("aarch64-linux-musl-cross");
+    if !aarch_musl_cross.is_dir() {
+        println!("downloading musl crosscompiler")
+        let bytes = reqwest::blocking::get("https://musl.cc/aarch64-linux-musl-cross.tgz").unwrap();
+        let buf = BufReader::new(bytes);
+        let tarfile = GzDecoder::new(buf);
+        let mut archive = Archive::new(tarfile);
+        archive.unpack(".").unwrap();
+    }
+}
+
+fn main() {
+    // #[cfg(target_arch = "aarch64")]
+    setup_aarch_crosscomp();
+}
