f96cdb843887a379f66cbe0fb50557e2db77e7aa
dvdsk <noreply@davidsk.dev>
2023-06-02 02:26:25 +0200

    fixed too long timeout in reachable check

--- main/src/diagnostics/reachable.rs
+++ main/src/diagnostics/reachable.rs
@@ -1,7 +1,9 @@
 use std::str::FromStr;
+use std::time::Duration;
 
 use color_eyre::{eyre, Help};
 use hyper::{body, Body, Response, StatusCode, Uri};
+use tokio::time::timeout;
 use tracing::debug;
 
 use crate::config::Config;
@@ -27,23 +29,28 @@ async fn check_response(resp: Response<Body>, key_auth: &str, domain: &str) -> e
     }
 }
 
+fn error_timeout(domain: &str) -> eyre::Result<()> {
+    // we explicitly do not instruct the user to turn the other application off
+    // in case it is a revers proxy/load balancer such as HAProxy. Another diagnostic
+    // will help them out once we can not bind to port 80.
+    Err(eyre::eyre!("Timed out reaching {domain}")
+        .note("Another server is getting traffic for external port 80, it is however not functioning")
+        .suggestion(format!("Check if port 80 is forwarded to a port on this machine. If it is configure {APP} to use that port with the `--port` option. If not forward port 80 to this machine")))
+}
+
 async fn check(path: &str, domain: &str, key_auth: &str) -> eyre::Result<()> {
     let url = format!("http://{domain}{path}");
     debug!("checking: {url}");
     let client = hyper::Client::new();
-    match client.get(Uri::from_str(&url).unwrap()).await {
-        Ok(resp) => check_response(resp, key_auth, domain).await,
-        Err(e) if e.is_timeout() => {
-            // we explicitly do not instruct the user to turn the other application off
-            // in case it is a revers proxy/load balancer such as HAProxy. Another diagnostic
-            // will help them out once we can not bind to port 80.
-            Err(eyre::eyre!("Timed out reaching {domain}")
-                .note("Another server is getting traffic for external port 80, it is however not functioning")
-                .suggestion(format!("Check if port 80 is forwarded to a port on this machine. If it is configure {APP} to use that port with the `--port` option. If not forward port 80 to this machine")))
-        }
-        Err(e) if e.is_connect() => Err(eyre::eyre!("Could not reach {APP} via {domain}")
-            .suggestion("Forword port 80 to this machine".to_string())),
-        Err(e) => unreachable!("reqwest error: {e:?}"),
+    let get = client.get(Uri::from_str(&url).unwrap());
+    let get = timeout(Duration::from_millis(50), get);
+    match get.await {
+        Ok(Ok(resp)) => check_response(resp, key_auth, domain).await,
+        Ok(Err(e)) if e.is_timeout() => error_timeout(domain),
+        Ok(Err(e)) if e.is_connect() => Err(eyre::eyre!("Could not reach {APP} via {domain}"))
+            .suggestion("Forward port 80 to this machine"),
+        Ok(Err(e)) => unreachable!("reqwest error: {e:?}"),
+        Err(_) => error_timeout(domain),
     }
 }
 
